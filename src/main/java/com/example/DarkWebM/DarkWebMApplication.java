ba04948ab97dbd3b875ee62a1d0f65b9439a801b
Omar Said <106676380+OMARARAB@users.noreply.github.com>
2025-04-01 22:54:10 +0200

    dockerfile

--- src/main/java/com/example/DarkWebM/DarkWebMApplication.java
+++ src/main/java/com/example/DarkWebM/DarkWebMApplication.java
@@ -2,12 +2,8 @@ package com.example.DarkWebM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.example.DarkWebM.Repository")  // Specify your repository package here
-@EntityScan(basePackages = "com.example.DarkWebM.Model")  // Specify your entity package here
 public class DarkWebMApplication {
 
 	public static void main(String[] args) {
